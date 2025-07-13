64a9f7f3872b34dc85564e0e9bedd612d3c03b11
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-14 01:07:34 +0800

    Create list.ts

---
+++ functions/api/nav-links/list.ts
@@ -0,0 +1,6 @@
+export async function onRequest(context) {
+  const { env } = context;
+  const sql = 'SELECT * FROM nav_links ORDER BY category, sort ASC, id DESC';
+  const res = await env.DB.prepare(sql).all();
+  return Response.json({ data: res.results });
+}
