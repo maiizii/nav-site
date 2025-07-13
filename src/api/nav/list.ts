f2c991fc5b595f2d4837beef63be28f250e23cf0
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-13 16:43:14 +0800

    Delete src/api directory

--- src/api/nav/list.ts
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context: any) {
-  const { env } = context;
-  const result = await env.DB.prepare(
-    'SELECT * FROM nav_links ORDER BY category, created_at DESC'
-  ).all();
-  return new Response(JSON.stringify({ success: true, data: result.results }), { headers: { 'Content-Type': 'application/json' } });
-}
