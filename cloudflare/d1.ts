af27aabe3d4b2edaa58fdaa4118380159eba23ac
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-13 15:50:47 +0800

    Update d1.ts

--- cloudflare/d1.ts
+++ cloudflare/d1.ts
@@ -1,3 +1,7 @@
 export async function queryD1(env: any, sql: string, params: any[] = []) {
   return await env.DB.prepare(sql).bind(...params).all();
 }
+
+export async function queryOne(env: any, sql: string, params: any[] = []) {
+  return await env.DB.prepare(sql).bind(...params).first();
+}
