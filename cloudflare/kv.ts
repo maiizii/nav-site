e70c3068c7616122c97a24feb68b38371a138e49
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-14 18:14:39 +0800

    Delete cloudflare/kv.ts

--- cloudflare/kv.ts
+++
@@ -1,7 +0,0 @@
-export async function kvGet(env: any, key: string) {
-  return await env.KV.get(key);
-}
-
-export async function kvSet(env: any, key: string, value: string) {
-  return await env.KV.put(key, value);
-}
