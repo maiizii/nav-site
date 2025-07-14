dc0c50ea0f5e086377b7dc357656fa77a69d906d
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-14 18:14:31 +0800

    Delete cloudflare/d1.ts

--- cloudflare/d1.ts
+++
@@ -1,7 +0,0 @@
-export async function queryD1(env: any, sql: string, params: any[] = []) {
-  return await env.DB.prepare(sql).bind(...params).all();
-}
-
-export async function queryOne(env: any, sql: string, params: any[] = []) {
-  return await env.DB.prepare(sql).bind(...params).first();
-}
