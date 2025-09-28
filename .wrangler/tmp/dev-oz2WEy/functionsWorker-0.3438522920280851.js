58e711e2fbfd96b56ea9b5cfa9ab786eaefa09d7
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-28 12:52:53 +0800

    Added functionsWorker-0.3438522920280851.js

--- .wrangler/tmp/dev-oz2WEy/functionsWorker-0.3438522920280851.js
+++ .wrangler/tmp/dev-oz2WEy/functionsWorker-0.3438522920280851.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-dWBKlj/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-B2Cgzi/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
@@ -16,7 +16,7 @@ globalThis.fetch = new Proxy(globalThis.fetch, {
   }
 });
 
-// .wrangler/tmp/pages-bcxQjo/functionsWorker-0.6753617048846816.mjs
+// .wrangler/tmp/pages-l0NAGS/functionsWorker-0.3438522920280851.mjs
 var __defProp2 = Object.defineProperty;
 var __name2 = /* @__PURE__ */ __name((target, value) => __defProp2(target, "name", { value, configurable: true }), "__name");
 function stripCfConnectingIPHeader2(input, init) {
@@ -1104,7 +1104,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-dWBKlj/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-B2Cgzi/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -1136,7 +1136,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-dWBKlj/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-B2Cgzi/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
@@ -1234,4 +1234,4 @@ export {
   __INTERNAL_WRANGLER_MIDDLEWARE__2 as __INTERNAL_WRANGLER_MIDDLEWARE__,
   middleware_loader_entry_default2 as default
 };
-//# sourceMappingURL=functionsWorker-0.6753617048846816.js.map
+//# sourceMappingURL=functionsWorker-0.3438522920280851.js.map
