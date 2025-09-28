4a215fd4e35e1c1be6d5f70c221cfbec248b0096
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-28 12:52:49 +0800

    Added middleware-loader.entry.ts

---
+++ .wrangler/tmp/bundle-B2Cgzi/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/home/project/.wrangler/tmp/pages-l0NAGS/functionsWorker-0.3438522920280851.mjs";
+				import * as __MIDDLEWARE_0__ from "/home/project/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/home/project/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/home/project/.wrangler/tmp/pages-l0NAGS/functionsWorker-0.3438522920280851.mjs";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
